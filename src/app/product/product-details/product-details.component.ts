0c15f1392c57dcc2360581c158e0e2bf579ca1c3
dominik-lewandowski <domin5892@gmail.com>
2020-03-17 20:48:22 +0100

    pause the carousel

--- src/app/product/product-details/product-details.component.ts
+++ src/app/product/product-details/product-details.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ProductItemModel} from '../../_models/product-item.model';
+import {NgbCarousel} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, AfterViewInit {
 
   product: ProductItemModel;
+
+  @ViewChild('carousel', {static: false}) carousel: NgbCarousel;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -22,6 +25,10 @@ export class ProductDetailsComponent implements OnInit {
       {uri: 'sdsf', alt: 'sds'},
       {uri: 'sdsf', alt: 'sds'}
     ];
+  }
+
+  ngAfterViewInit(): void {
+    this.carousel.pause();
   }
 
 }
