e59888375075433972aa29fc2e6f73acd03e39c4
Dominik <domin5892@gmail.com>
2020-04-11 19:29:33 +0200

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   apiUrl: 'https://api.tlo-devs.com/stag/11sevendome/',
-  imgUrl: 'https://api.tlo-devs.com/stag/11sevendome/',
+  imgUrl: 'https://img.tlo-devs.com/',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https://api.tlo-devs.com/',
-  imgUrl: 'https://api.tlo-devs.com/img/',
+  apiUrl: 'https://api.tlo-devs.com/stag/11sevendome/',
+  imgUrl: 'https://img.tlo-devs.com/',
   production: false
 };
 
