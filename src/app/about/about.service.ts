80db35ca547ae535d9f6d90f05584f2fb7e87b1e
Dominik <domin5892@gmail.com>
2020-09-20 13:17:05 +0200

    implement the about endpoint

--- src/app/about/about.service.ts
+++ src/app/about/about.service.ts
@@ -6,40 +6,12 @@ import {AboutModel} from '../_models/about/about.model';
 @Injectable()
 export class AboutService {
 
+  private aboutUrl = 'content/about/';
+
   constructor(private rest: RestService) {
   }
 
   content$(): Observable<AboutModel> {
-    return of({
-      img: '../../assets/images/about.jpg',
-      about: [
-        {
-          title: 'Hauptberuf',
-          text: 'Videoeditor/Fotograf'
-        },
-        {
-          title: 'Zahlreiche Erfahrungen als',
-          text: 'Kameramann, Lichtassistent, Cutter'
-        }
-      ],
-      vita: [
-        {
-          year: '2018',
-          text: 'Ausbildung als Mediengestalter Bild und Ton'
-        },
-        {
-          year: '2018',
-          text: 'Studium Maschinenbau'
-        },
-        {
-          year: '2017',
-          text: 'Abitur (Brandenburg)'
-        },
-        {
-          year: '2014',
-          text: 'FSJ als Krankenpfleger'
-        }
-      ]
-    });
+    return this.rest.get<AboutModel>(this.aboutUrl);
   }
 }
